54dea22c5c70471b327f82d6d7e8e96fc8b1e1b4
skariuki <stanleykariuki@gmail.com>
2017-08-22 01:30:38 -0400

    updated parser

--- ast/ast.go
+++ ast/ast.go
@@ -241,3 +241,28 @@ func (fe *FunctionExpression) String() string {
 
 	return out.String()
 }
+
+type CallExpression struct {
+	Token     token.Token
+	Function  Expression
+	Arguments []Expression
+}
+
+func (ce *CallExpression) expressionNode()      {}
+func (ce *CallExpression) TokenLiteral() string { return ce.Token.Literal }
+func (ce *CallExpression) String() string {
+	var out bytes.Buffer
+
+	args := []string{}
+
+	for _, arg := range ce.Arguments {
+		args = append(args, arg.String())
+	}
+
+	out.WriteString(ce.Function.String())
+	out.WriteString("(")
+	out.WriteString(strings.Join(args, ", "))
+	out.WriteString(")")
+
+	return out.String()
+}
